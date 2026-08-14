2477fb383be2176cf49d8d6d5e3b536e142c3973
pawelxx1212 <pawel.bukowiecki2016@gmail.com>
2026-08-14 16:47:57 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,12 +1,5 @@
 if game.PlaceId == 115915240028498 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Pawelxx1212/Roblox-Mod-Menus/refs/heads/main/Games/I%20Heard%20It%20Too%20Remastered"))()
-
-    print("working")
-    print("Test code is running.")
-
-elseif game.PlaceId == 12355435345 then
-    loadstring(somethingblablabla)()
-
-else
-    print("❌ Place ID does not match either place.")
+    loadstring(game:HttpGet(
+        "https://raw.githubusercontent.com/Pawelxx1212/Roblox-Mod-Menus/refs/heads/main/Games/I%20Heard%20It%20Too%20Remastered"
+    ))()
 end
