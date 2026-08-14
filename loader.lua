5fb6a135189152a5cb2e9f5baedac857dfc36229
pawelxx1212 <pawel.bukowiecki2016@gmail.com>
2026-08-14 16:45:52 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,7 +1,7 @@
 if game.PlaceId == 115915240028498 then
-    print("✅ Place ID matches!")
-
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Pawelxx1212/Roblox-Mod-Menus/refs/heads/main/Games/I%20Heard%20It%20Too%20Remastered"))()
+
+    print("working")
     print("Test code is running.")
 
 elseif game.PlaceId == 12355435345 then
