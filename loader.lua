d681073c992a23e5c40f34c41735824e27d1deec
pawelxx1212 <pawel.bukowiecki2016@gmail.com>
2026-08-14 18:48:29 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,6 @@
 local games = {
     [115915240028498] = "https://raw.githubusercontent.com/Pawelxx1212/Roblox-Mod-Menus/refs/heads/main/Games/I%20Heard%20It%20Too%20Remastered",
-    [126323186948264] = "https://raw.githubusercontent.com/Pawelxx1212/Roblox-Mod-Menus/refs/heads/main/Games/Paradox"
+    [126323186948264] = "https://raw.githubusercontent.com/Pawelxx1212/Roblox-Mod-Menus/refs/heads/main/Games/Paradox.lua"
 }
 
 local url = games[game.PlaceId]
