da9d5823f56860e28c91abce49fa699e699ba882
pawelxx1212 <pawel.bukowiecki2016@gmail.com>
2026-08-14 16:51:07 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,4 +2,12 @@ if game.PlaceId == 115915240028498 then
     loadstring(game:HttpGet(
         "https://raw.githubusercontent.com/Pawelxx1212/Roblox-Mod-Menus/refs/heads/main/Games/I%20Heard%20It%20Too%20Remastered"
     ))()
+
+elseif game.PlaceId == 1234567890 then
+    loadstring(game:HttpGet(
+        "https://example.com/your-second-script.lua"
+    ))()
+
+else
+    warn("This game is not supported.")
 end
