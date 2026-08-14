0e55f7f424b089568a2fdf81ae8cbede21ce8f39
pawelxx1212 <pawel.bukowiecki2016@gmail.com>
2026-08-14 19:02:59 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,34 +1,13 @@
-local placeId = tostring(game.PlaceId)
+if game.PlaceId == 115915240028498 then
+    loadstring(game:HttpGet(
+        "https://raw.githubusercontent.com/Pawelxx1212/Roblox-Mod-Menus/refs/heads/main/Games/I%20Heard%20It%20Too%20Remastered"
+    ))()
 
-print("Detected PlaceId:", placeId)
-
-if placeId == "115915240028498" then
-    print("Detected I Heard It Too")
-
-    local success, err = pcall(function()
-        loadstring(game:HttpGet(
-            "https://raw.githubusercontent.com/Pawelxx1212/Roblox-Mod-Menus/refs/heads/main/Games/I%20Heard%20It%20Too%20Remastered"
-        ))()
-    end)
-
-    if not success then
-        warn("I Heard It Too error:", err)
-    end
-
-elseif placeId == "126323186948264" then
-    print("Detected Paradox")
-
-    local success, err = pcall(function()
-        loadstring(game:HttpGet(
-            "https://raw.githubusercontent.com/Pawelxx1212/Roblox-Mod-Menus/refs/heads/main/Games/Paradox"
-        ))()
-    end)
-
-    if not success then
-        warn("Paradox error:", err)
-    end
+elseif game.PlaceId == 95959136210771 then
+    loadstring(game:HttpGet(
+        "https://raw.githubusercontent.com/Pawelxx1212/Roblox-Mod-Menus/refs/heads/main/Games/Paradox"
+    ))()
 
 else
-    warn("This game is not supported!")
-    warn("PlaceId detected:", placeId)
+    warn("This game is not supported.")
 end
