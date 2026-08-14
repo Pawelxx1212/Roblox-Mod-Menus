4c6f54185fe38062ba2be0c3aedd826b970911a6
pawelxx1212 <pawel.bukowiecki2016@gmail.com>
2026-08-14 16:44:00 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,12 +1,12 @@
-local gameId = 115915240028498
-local scriptUrl = "https://raw.githubusercontent.com/Pawelxx1212/Roblox-Mod-Menus/refs/heads/main/Games/I%20Heard%20It%20Too%20Remastered"
+if game.PlaceId == 115915240028498 then
+    print("✅ Place ID matches!")
 
-if game.PlaceId == gameId then
-    local success, result = pcall(function()
-        return loadstring(game:HttpGet(scriptUrl))()
-    end)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Pawelxx1212/Roblox-Mod-Menus/refs/heads/main/Games/I%20Heard%20It%20Too%20Remastered"))()
+    print("Test code is running.")
 
-    if not success then
-        warn("Failed to load script:", result)
-    end
+elseif game.PlaceId == 12355435345 then
+    loadstring(somethingblablabla)()
+
+else
+    print("❌ Place ID does not match either place.")
 end
