105e7d9fec2d60e8dd5ff055b1f60696f819400b
pawelxx1212 <pawel.bukowiecki2016@gmail.com>
2026-08-14 18:56:09 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,9 +3,9 @@ if game.PlaceId == 115915240028498 then
         "https://raw.githubusercontent.com/Pawelxx1212/Roblox-Mod-Menus/refs/heads/main/Games/I%20Heard%20It%20Too%20Remastered"
     ))()
 
-elseif game.PlaceId == 1234567890 then
+elseif game.PlaceId == 126323186948264 then
     loadstring(game:HttpGet(
-        "https://example.com/your-second-script.lua"
+        "https://raw.githubusercontent.com/Pawelxx1212/Roblox-Mod-Menus/refs/heads/main/Games/Paradox"
     ))()
 
 else
