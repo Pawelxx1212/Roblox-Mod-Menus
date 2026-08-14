6b8ccc3564c096f16511dee0a3cc2829a5595197
pawelxx1212 <pawel.bukowiecki2016@gmail.com>
2026-08-14 18:45:24 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,13 +1,26 @@
-if game.PlaceId == 115915240028498 then
-    loadstring(game:HttpGet(
-        "https://raw.githubusercontent.com/Pawelxx1212/Roblox-Mod-Menus/refs/heads/main/Games/I%20Heard%20It%20Too%20Remastered"
-    ))()
+local games = {
+    [115915240028498] = "https://raw.githubusercontent.com/Pawelxx1212/Roblox-Mod-Menus/refs/heads/main/Games/I%20Heard%20It%20Too%20Remastered.lua",
+    [126323186948264] = "https://raw.githubusercontent.com/Pawelxx1212/Roblox-Mod-Menus/refs/heads/main/Games/Paradox.lua"
+}
 
-elseif game.PlaceId == 126323186948264 then
-    loadstring(game:HttpGet(
-        "https://raw.githubusercontent.com/Pawelxx1212/Roblox-Mod-Menus/refs/heads/main/Games/Paradox"
-    ))()
+local url = games[game.PlaceId]
 
+if url then
+    local success, result = pcall(function()
+        return game:HttpGet(url)
+    end)
+
+    if success then
+        local func, err = loadstring(result)
+
+        if func then
+            func()
+        else
+            warn("Failed to compile script: " .. tostring(err))
+        end
+    else
+        warn("Failed to download script: " .. tostring(result))
+    end
 else
-    warn("This game is not supported.")
+    warn("This game is not supported. PlaceId: " .. tostring(game.PlaceId))
 end
